bbf6953cace35c26d419f60c1f97a573eac33e7e
Raman Kandratchyk <kondrator@gmail.com>
2023-05-15 17:03:50 +0300

    feat: start collect products data

---
+++ src/data/products.ts
@@ -0,0 +1,40 @@
+const PRODUCTS = [
+  {
+    id: 1,
+    title: 'Spathiphyllum',
+    description:
+      'With its elegant and timeless appearance, the peace lily (Spathiphyllum) blends into any room. It is ideal for inexperienced hobby gardeners and comes in a variety of different heights and leaf colours.',
+    price: 20.0,
+    discountPercentage: 10,
+    rating: 4.75,
+    stock: 15,
+    latinName: 'Spathiphyllum',
+    category: 'Decorative leafy plants',
+    thumbnail: '../assets/images/spathiphyllum/spathiphyllum-thumb.webp',
+    images: [
+      '../assets/images/spathiphyllum/spathiphyllum-1.webp',
+      '../assets/images/spathiphyllum/spathiphyllum-2.webp',
+      '../assets/images/spathiphyllum/spathiphyllum-3.webp',
+    ],
+  },
+  {
+    id: 2,
+    title: 'Zamioculcas zamielifolia',
+    description:
+      'In recent years, the Zamioculcas zamielifolia plant has become a must-have designer plant. Since it was offered on the market almost 20 years ago, it has become a firm favourite in offices, medical practices and designer apartments. It is the epitome of modern design as far as houseplants are concerned.',
+    price: 18.97,
+    discountPercentage: 10,
+    rating: 4.85,
+    stock: 24,
+    latinName: 'Zamioculcas zamiifolia',
+    category: 'Decorative leafy plants',
+    thumbnail: '../assets/images/spathiphyllum/zamioculcas-thumb.webp',
+    images: [
+      '../assets/images/spathiphyllum/zamioculcas-1.webp',
+      '../assets/images/spathiphyllum/zamioculcas-2.webp',
+      '../assets/images/spathiphyllum/zamioculcas-3.webp',
+    ],
+  },
+];
+
+export default PRODUCTS;
