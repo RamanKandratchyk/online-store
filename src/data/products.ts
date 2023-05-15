08b0103d18575e809a1ab297d285c9edb59b523e
Raman Kandratchyk <kondrator@gmail.com>
2023-05-15 23:09:01 +0300

    feat: add products data (6 items done)

--- src/data/products.ts
+++ src/data/products.ts
@@ -35,6 +35,78 @@ const PRODUCTS = [
       '../assets/images/spathiphyllum/zamioculcas-3.webp',
     ],
   },
+  {
+    id: 3,
+    title: 'Chlorophytum',
+    description:
+      'Chlorophytum comosum is the most common spider plant species to be kept as a houseplant. While there are other spider plant species as well, they are harder to come by and more difficult to care for.',
+    price: 5.46,
+    discountPercentage: 5,
+    rating: 4.34,
+    stock: 16,
+    latinName: 'Chlorophytum',
+    category: 'Decorative leafy plants',
+    thumbnail: '../assets/images/chlorophytum/chlorophytum-thumb.webp',
+    images: [
+      '../assets/images/chlorophytum/chlorophytum-1.webp',
+      '../assets/images/chlorophytum/chlorophytum-2.webp',
+      '../assets/images/chlorophytum/chlorophytum-3.webp',
+    ],
+  },
+  {
+    id: 4,
+    title: 'Calathea crocata',
+    description:
+      'Eternal Flame plants, also known as saffron-coloured calatheas, have a reputation for being difficult to care for. That said, simply understanding a few of this plant’s requirements will help you to create the perfect conditions in which it will flourish.',
+    price: 17.75,
+    discountPercentage: 7,
+    rating: 4.48,
+    stock: 6,
+    latinName: 'Calathea crocata',
+    category: 'Decorative leafy plants',
+    thumbnail: '../assets/images/calathea/calathea-thumb.webp',
+    images: [
+      '../assets/images/calathea/calathea-1.webp',
+      '../assets/images/calathea/calathea-2.webp',
+      '../assets/images/calathea/calathea-3.webp',
+    ],
+  },
+  {
+    id: 5,
+    title: 'Sansevieria',
+    description:
+      'The snake plant (Sansevieria) is an air purifier and style object in offices, bedrooms and homes. The versatile survival artist shows himself in the most beautiful shades.',
+    price: 11.0,
+    discountPercentage: 5,
+    rating: 4.15,
+    stock: 11,
+    latinName: 'Sansevieria',
+    category: 'Decorative leafy plants',
+    thumbnail: '../assets/images/sansevieria/sansevieria-thumb.webp',
+    images: [
+      '../assets/images/sansevieria/sansevieria-1.webp',
+      '../assets/images/sansevieria/sansevieria-2.webp',
+      '../assets/images/sansevieria/sansevieria-3.webp',
+    ],
+  },
+  {
+    id: 6,
+    title: 'Mini monstera',
+    description:
+      'The mini monstera dons impressively glossy green, fenestrated leaves and can be grown as a climbing or hanging plant.',
+    price: 20.32,
+    discountPercentage: 10,
+    rating: 4.45,
+    stock: 19,
+    latinName: 'Rhaphidophora tetrasperma',
+    category: 'Decorative leafy plants',
+    thumbnail: '../assets/images/monstera/monstera-thumb.webp',
+    images: [
+      '../assets/images/monstera/monstera-1.webp',
+      '../assets/images/monstera/monstera-2.webp',
+      '../assets/images/monstera/monstera-3.webp',
+    ],
+  },
 ];
 
 export default PRODUCTS;
