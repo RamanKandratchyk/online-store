8b8eeedeb7c43f22415cdb5f98ab53939e3bf4b3
Raman Kandratchyk <kondrator@gmail.com>
2023-06-16 22:14:18 +0300

    feat: complete markup dual sliders

--- src/components/view/main/main.ts
+++ src/components/view/main/main.ts
@@ -1,3 +1,5 @@
+// change logo colours code start
+
 function getRandomNumber(range: number): number {
   return Math.floor(Math.random() * range);
 }
@@ -22,3 +24,121 @@ function changeLogoColours(): void {
 }
 
 setInterval(changeLogoColours, 1000);
+
+// change logo colours code end
+
+// dual range price slider code start
+
+const priceFromSlider = document.getElementById('price-from') as HTMLInputElement;
+const priceToSlider = document.getElementById('price-to') as HTMLInputElement;
+const priceFromText = document.getElementById('price-from-text') as HTMLDivElement;
+const priceToText = document.getElementById('price-to-text') as HTMLDivElement;
+
+function getParsed(currentFrom: HTMLInputElement, currentTo: HTMLInputElement) {
+  const from = parseInt(currentFrom.value, 10);
+  const to = parseInt(currentTo.value, 10);
+  return [from, to];
+}
+
+function fillSlider(
+  from: HTMLInputElement,
+  to: HTMLInputElement,
+  sliderColor: string,
+  rangeColor: string,
+  controlSlider: HTMLInputElement
+) {
+  const rangeDistance = +to.max - +to.min;
+  const fromPosition = +from.value - +to.min;
+  const toPosition = +to.value - +to.min;
+  const sliderToFill = controlSlider;
+  sliderToFill.style.backgroundImage = `linear-gradient(
+    to right,
+    ${sliderColor} 0%,
+    ${sliderColor} ${(fromPosition / rangeDistance) * 100}%,
+    ${rangeColor} ${(fromPosition / rangeDistance) * 100}%,
+    ${rangeColor} ${(toPosition / rangeDistance) * 100}%, 
+    ${sliderColor} ${(toPosition / rangeDistance) * 100}%, 
+    ${sliderColor} 100%)`;
+}
+
+function setToggleAccessible(currentTarget: HTMLInputElement) {
+  const sliderToToggle = currentTarget;
+  if (Number(currentTarget.value) <= 0) {
+    sliderToToggle.style.zIndex = '2';
+  } else {
+    sliderToToggle.style.zIndex = '0';
+  }
+}
+
+function controlPriceFromSlider(fromSliderVar: HTMLInputElement, toSliderVar: HTMLInputElement) {
+  const [from, to] = getParsed(fromSliderVar, toSliderVar);
+  fillSlider(fromSliderVar, toSliderVar, '#C6C6C6', '#46f0a9', priceToSlider);
+  if (from > to) {
+    priceFromSlider.value = `${to}`;
+    priceFromText.innerText = `€${to.toFixed(2)}`;
+  } else {
+    priceFromSlider.value = `${from}`;
+    priceFromText.innerText = `€${from.toFixed(2)}`;
+  }
+}
+
+function controlPriceToSlider(fromSliderVar: HTMLInputElement, toSliderVar: HTMLInputElement) {
+  const [from, to] = getParsed(fromSliderVar, toSliderVar);
+  fillSlider(fromSliderVar, toSliderVar, '#C6C6C6', '#46f0a9', priceToSlider);
+  setToggleAccessible(toSliderVar);
+  if (from <= to) {
+    priceToSlider.value = `${to}`;
+    priceToText.innerText = `€${to.toFixed(2)}`;
+  } else {
+    priceToSlider.value = `${from}`;
+    priceToText.innerText = `€${from.toFixed(2)}`;
+  }
+}
+
+fillSlider(priceFromSlider, priceToSlider, '#C6C6C6', '#25daa5', priceToSlider);
+setToggleAccessible(priceToSlider);
+
+priceFromSlider.oninput = () => controlPriceFromSlider(priceFromSlider, priceToSlider);
+priceToSlider.oninput = () => controlPriceToSlider(priceFromSlider, priceToSlider);
+
+// dual range price slider code end
+
+// dual range stock slider code start
+
+const stockFromSlider = document.getElementById('stock-from') as HTMLInputElement;
+const stockToSlider = document.getElementById('stock-to') as HTMLInputElement;
+const stockFromText = document.getElementById('stock-from-text') as HTMLDivElement;
+const stockToText = document.getElementById('stock-to-text') as HTMLDivElement;
+
+function controlStockFromSlider(fromSliderVar: HTMLInputElement, toSliderVar: HTMLInputElement) {
+  const [from, to] = getParsed(fromSliderVar, toSliderVar);
+  fillSlider(fromSliderVar, toSliderVar, '#C6C6C6', '#46f0a9', stockToSlider);
+  if (from > to) {
+    stockFromSlider.value = `${to}`;
+    stockFromText.innerText = `${to}`;
+  } else {
+    stockFromSlider.value = `${from}`;
+    stockFromText.innerText = `${from}`;
+  }
+}
+
+function controlStockToSlider(fromSliderVar: HTMLInputElement, toSliderVar: HTMLInputElement) {
+  const [from, to] = getParsed(fromSliderVar, toSliderVar);
+  fillSlider(fromSliderVar, toSliderVar, '#C6C6C6', '#46f0a9', stockToSlider);
+  setToggleAccessible(toSliderVar);
+  if (from <= to) {
+    stockToSlider.value = `${to}`;
+    stockToText.innerText = `${to}`;
+  } else {
+    stockToSlider.value = `${from}`;
+    stockToText.innerText = `${from}`;
+  }
+}
+
+fillSlider(stockFromSlider, stockToSlider, '#C6C6C6', '#25daa5', stockToSlider);
+setToggleAccessible(stockToSlider);
+
+stockFromSlider.oninput = () => controlStockFromSlider(stockFromSlider, stockToSlider);
+stockToSlider.oninput = () => controlStockToSlider(stockFromSlider, stockToSlider);
+
+// dual range stock slider code end
