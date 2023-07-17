9c0b76fed60ba8e2e7b87b6247f5c227555f00e4
Raman Kandratchyk <kondrator@gmail.com>
2023-07-17 15:34:52 +0300

    feat: add searchFilter

---
+++ src/components/view/filters/searchFilter.ts
@@ -0,0 +1,32 @@
+// import { app } from '../../../index';
+import { STATE, DEFAULT_STATE } from '../../state/state';
+
+class SearchFilter {
+  search: string = DEFAULT_STATE.filters.search;
+
+  private getState(): void {
+    this.search = STATE.filters.search;
+  }
+
+  render(): string {
+    this.getState();
+    return `
+      <div class="sort-products__search-bar">
+        <input type="search" class="search-bar__input ${
+          this.search !== '' ? 'search-active' : ''
+        }" placeholder="Search product" ${this.search ? `value="${this.search}"` : ''} />
+      </div>
+    `;
+  }
+
+  listener(): void {
+    const el = document.querySelector('.search-bar__input') as HTMLInputElement;
+    if (this.search !== '') {
+      el.focus();
+      el.selectionStart = el.value.length;
+    }
+    // el.oninput = () => app.controller.appStateControl('search', el.value); //! uncomment later
+  }
+}
+
+export default SearchFilter;
